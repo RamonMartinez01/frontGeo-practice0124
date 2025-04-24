f1c1a65e7143c4acf2fbef9e56a4732f7cbda460
RamonMartinez01 <mafrtinezcjr1@gmail.com>
2025-04-23 21:03:40 -0700

    cambia a API URL de Koyeb

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = 'https://d21qwwdkmqepg3.cloudfront.net/api/escuelas/'
+/*const API_BASE_URL = 'https://d21qwwdkmqepg3.cloudfront.net/api/escuelas/'*/
+const API_BASE_URL = 'https://crude-maryjo-ramonmartinez01-d2a8e9de.koyeb.app/api/escuelas'
 export default API_BASE_URL
